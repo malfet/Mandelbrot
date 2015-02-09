e96e5d4e3aee3991e907a1ef2d5fdee6662fbfc6
Nikita Shulga <nikita.shulga@gmail.com>
2015-02-08 19:22:32 -0800

    Initial Commit

---
+++ Mandelbrot/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Mandelbrot
+//
+//  Created by Shulga Nikita on 2015/02/08.
+//  Copyright (c) 2015年 Home. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
